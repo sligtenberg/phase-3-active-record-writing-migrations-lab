bc14e8e8942ae63b4e11e32c790892153eedfcb8
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-28 13:02:41 -0500

    commit tree 5e6e832a7c32079504b6e789499620d0bc0ef767

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,18 @@
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+
+
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
